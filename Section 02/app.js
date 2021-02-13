0955caceaabad549320d21e2f8b5c0ae7c746940
MaćKo <maciej.kostrzycki@gmail.com>
2021-02-13 18:23:22 +0100

    Play with console

---
+++ Section 02/app.js
@@ -0,0 +1,21 @@
+// console
+
+console.time('timer01');
+
+console.log('Hello World');
+
+console.log(123);
+
+console.log(true);
+
+console.log([1,2,3,4]);
+
+console.log({a:1, b:2});
+
+console.table({a:1, b:2});
+
+console.error('Error message');
+
+console.warn('Warning message');
+
+console.timeEnd('timer01');
